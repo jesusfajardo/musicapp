fbb465b3d087dcb9845f4488519f6d196e92dad5
Jesus <fajardosullcajesus@gmail.com>
2020-10-22 01:19:27 -0400

    error revisado

--- database/connect.js
+++ database/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Fastfood", {
+mongoose.connect("mongodb://172.25.0.2:27017/restaurantapp", {
   useNewUrlParser: true
 }).then(()=>{
   console.log('connexion a mongodb existosa');
